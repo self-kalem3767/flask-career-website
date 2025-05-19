f7f0cea890c79d4a8fbf415f4373f5a404be10d3
self-kalem3767 <azaadg295@gmail.com>
2025-05-19 10:07:59 +0500

    added requrements.txt file

--- app.py
+++ app.py
@@ -1,12 +1,47 @@
 from flask import Flask, render_template
+from flask import jsonify
 
 
 # Web Development with Python Flask
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Lahore, Pakistan',
+        'salary': 'Rs.10,000, 00',
+        'job-type': 'onsite'
+    },
+    {
+        'id': 2,
+        'title': 'Data Scientist',
+        'location': 'Islamabad, Pakistan',
+        'salary': 'Rs. 15,000, 00',
+        'job-type': 'onsite'
+    },
+    {
+        'id': 3,
+        'title': 'Frontend Engineer',
+        'location': 'Faslabad, Pakistan',
+        'job-type': 'Remote'
+    },
+    {
+        'id': 4,
+        'title': 'Backend Engineer',
+        'location': 'San Francisco, USA',
+        'salary': '$12000',
+        'job-type': 'Remote'
+    }
+]
+
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS, company_name='Devsinc')
+
+@app.route('/api/jobs')
+def job_list():
+    return jsonify(JOBS)
 
 
 if __name__ == '__main__':
